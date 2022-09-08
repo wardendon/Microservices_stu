ce533b580c3390b1581ebd95a80f6fcd4ad62aba
w2gd <W2GDong@gmail.com>
2022-09-08 10:27:27 +0800

    OpenFeign: 异常排查、超时判断、服务降级

--- share-project/content-service/src/main/java/top/w2gd/content/ContentCenterApplication.java
+++ share-project/content-service/src/main/java/top/w2gd/content/ContentCenterApplication.java
@@ -2,13 +2,16 @@ package top.w2gd.content;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author w2gd
  */
 @SpringBootApplication
-@EnableFeignClients(clients = {top.w2gd.content.openfeign.UserService.class})
+// @EnableFeignClients(clients = {top.w2gd.content.openfeign.UserService.class})
+@EnableFeignClients(basePackages = {"top.w2gd.content"})
 public class ContentCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(ContentCenterApplication.class,args);
--- share-project/user-service/src/main/java/top/w2gd/user/controller/UserController.java
+++ share-project/user-service/src/main/java/top/w2gd/user/controller/UserController.java
@@ -2,9 +2,11 @@ package top.w2gd.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import top.w2gd.user.common.ResponseResult;
+import top.w2gd.user.common.ResultCode;
 import top.w2gd.user.domain.entity.User;
 import top.w2gd.user.domain.entity.dto.UserDto;
 import top.w2gd.user.service.UserService;
@@ -21,8 +23,17 @@ public class UserController {
 
     @GetMapping("{id}")
     public ResponseResult getUserById(@PathVariable Integer id) {
-        System.out.println("getUser......");
-        return ResponseResult.success(userService.findById(id));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        val user  = userService.findById(id);
+        if (user != null) {
+            return ResponseResult.success(user);
+        } else {
+            return ResponseResult.failure(ResultCode.RESULT_CODE_DATA_NONE);
+        }
     }
 
     @PostMapping("/login")
